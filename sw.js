fff2952d34b759c5fbdca3d99fa10e7ed84b8460
ManuelLegend <manuelgdar@gmail.com>
2026-06-27 22:27:34 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "quiniela-pwa-v3";
+﻿const CACHE_NAME = "quiniela-pwa-v3";
 const URLS = [
   "./",
   "./index.html",
@@ -52,3 +52,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
